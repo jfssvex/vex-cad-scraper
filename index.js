ad09e7eb267d60fba25b19b86cb121b4b52b60b5
Jason Huang <jasonhuang20035@gmail.com>
2020-09-27 12:44:32 -0400

    add item url scraping

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+/**
+ * Check if element is vitible
+ * @param {puppeteer.Page} page Page to check element on
+ * @param {string} cssSelector
+ */
+const isElementClickable = async (page, cssSelector) => {
+  let clickable = true;
+
+  // If selector is gone, set clickable to false
+  await page
+    .waitForSelector(cssSelector, { visible: true, timeout: 2000 })
+    .catch(() => {
+      clickable = false;
+    });
+
+  // If button is disabled, set clickable to false
+  if (await page.$eval(cssSelector, (item) => item.disabled)) {
+    clickable = false;
+  }
+  return clickable;
+};
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
   const page = await browser.newPage();
 
   // Download V5 Parts
-  await page.goto('https://www.vexrobotics.com/v5/products/view-all');
-  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  await page.goto("https://www.vexrobotics.com/v5/products/view-all", {
+    waitUntil: "networkidle2",
+  });
 
+  // Close shipping notice modal
+  await page.click("a.js-modal-close");
   // Load all the pages
-  while (await page.$('button.ais-infinite-hits--showmoreButton') !== null) {
-      await page.click('button.ais-infinite-hits--showmoreButton');
-      await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  let loadMoreSelector = "button.ais-infinite-hits--showmoreButton";
+  let loadMoreVisible = await isElementClickable(page, loadMoreSelector);
+  while (loadMoreVisible) {
+    await page.click(loadMoreSelector).catch((err) => {
+      console.error(err);
+    });
+    loadMoreVisible = await isElementClickable(page, loadMoreSelector);
   }
 
-  await page.screenshot({path: 'example.png'});
-
+  console.log(await page.$$eval(".result", results => results.map(result => result.href)));
 
   await browser.close();
 })();
